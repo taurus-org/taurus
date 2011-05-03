cec6296a60c458c30bece32100d79c3d8309df1f
tiagocoutinho <tiagocoutinho@users.sourceforge.net>
2011-05-03 08:45:34 +0000

    made possible to compile with both tango 7.1 and 7.2
    git-svn-id: file:///home/cpascual/src/sdnongit/svnbck/sardana/share/Sardana/trunk@16765 c480fdf4-a248-4bb0-8e4a-34cd1ef68f4f

--- Pool/src/MeasurementGroup/MeasurementGroup.cpp
+++ Pool/src/MeasurementGroup/MeasurementGroup.cpp
@@ -1894,7 +1894,11 @@ void MeasurementGroup::remove_unwanted_dyn_attr_from_device()
         for(vector<string>::size_type rem_idx = 0; 
             rem_idx < remove_attr_list.size(); rem_idx++)
         {
+#if TANGO_VERSION_HEX < 0x07020000
             dev_attr->remove_attribute(remove_attr_list[rem_idx], true);
+#else
+            dev_attr->remove_attribute(remove_attr_list[rem_idx], true);
+#endif
         }
     }
 
