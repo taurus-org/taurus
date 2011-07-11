2d71a8ac5a34fc436d9d0cfd1dd27e6820f8f90d
cpascual <cpascual@users.sourceforge.net>
2011-07-11 13:22:16 +0000

    Added designerplugin for guiqwt
    git-svn-id: http://svn.code.sf.net/p/tango-cs/code/gui/taurus/trunk@17209 4e9c00fd-8f2e-0410-aa12-93ce3db5e235

--- lib/taurus/qt/qtdesigner/extraguiqwtplugin.py
+++ lib/taurus/qt/qtdesigner/extraguiqwtplugin.py
@@ -1,14 +1,29 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright © 2009-2010 CEA
-# Pierre Raybaut
-# Licensed under the terms of the CECILL License
-# (see guiqwt/__init__.py for details)
+#!/usr/bin/env python
+
+#############################################################################
+##
+## This file is part of Taurus, a Tango User Interface Library
+## 
+## http://www.tango-controls.org/static/taurus/latest/doc/html/index.html
+##
+## Copyright 2011 CELLS / ALBA Synchrotron, Bellaterra, Spain
+## 
+## Taurus is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Lesser General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+## 
+## Taurus is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+## 
+## You should have received a copy of the GNU Lesser General Public License
+## along with Taurus.  If not, see <http://www.gnu.org/licenses/>.
+##
+#############################################################################
 
 """
-guiqwtplugin
-============
-
 guiqwt widgets plugins for Qt Designer
 """
 
@@ -19,3 +34,4 @@ PlotPlugin = create_qtdesigner_plugin("guiqwt", "guiqwt.plot", "CurveWidget",
 
 ImagePlotPlugin = create_qtdesigner_plugin("guiqwt", "guiqwt.plot", "ImageWidget",
                                   icon="image.png")
+
