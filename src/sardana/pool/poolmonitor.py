37f917c5e4c85d71c3a4e67e33ac679e5c54ef09
tiagocoutinho <tiagocoutinho@users.sourceforge.net>
2011-10-04 16:30:29 +0000

    general development
    git-svn-id: file:///home/cpascual/src/sdnongit/svnbck/sardana/share/Sardana/trunk@18047 c480fdf4-a248-4bb0-8e4a-34cd1ef68f4f

--- src/sardana/pool/poolmonitor.py
+++ src/sardana/pool/poolmonitor.py
@@ -50,7 +50,7 @@ class PoolMonitor(Logger, threading.Thread):
     MIN_THREADS =  1
     MAX_THREADS = 10
     
-    def __init__(self, pool, name='PoolMonitor', period=.1, min_sleep=1.0,
+    def __init__(self, pool, name='PoolMonitor', period=5.0, min_sleep=1.0,
                  auto_start=True):
         Logger.__init__(self, name)
         threading.Thread.__init__(self, name=name)
