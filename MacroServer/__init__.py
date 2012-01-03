d0e070b79000a99dc9c194583565d9aaa6eb7080
tiagocoutinho <tiagocoutinho@users.sourceforge.net>
2012-01-03 10:43:00 +0000

    trunk cleanup
    git-svn-id: file:///home/cpascual/src/sdnongit/svnbck/sardana/share/Sardana/trunk@18803 c480fdf4-a248-4bb0-8e4a-34cd1ef68f4f

--- MacroServer/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-from MacroServer import *
-
-__doc__ = MacroServer.__doc__
--- MacroServer/scan/__init__.py
+++
@@ -1,5 +0,0 @@
-from datarecorder import *
-from scandata import *
-from output import *
-from sharedmemory import SharedMemoryRecorder
-from storage import FileRecorder
