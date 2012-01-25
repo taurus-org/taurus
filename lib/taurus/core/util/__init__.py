d74c7cc79c84c2cd9633606c1df9524665f0038a
tiagocoutinho <tiagocoutinho@users.sourceforge.net>
2012-01-25 10:26:03 +0000

    since taurus depends on lxml it doesn't make sence to try to load etree from other sources
    git-svn-id: http://svn.code.sf.net/p/tango-cs/code/gui/taurus/trunk@19055 4e9c00fd-8f2e-0410-aa12-93ce3db5e235

--- lib/taurus/core/util/__init__.py
+++ lib/taurus/core/util/__init__.py
@@ -61,26 +61,7 @@ from .user import *
 
 import eventfilters
 
-try:
-    from lxml import etree
-except ImportError:
-    try:
-        # Python 2.5
-        import xml.etree.cElementTree as etree
-    except ImportError:
-        try:
-            # Python 2.5
-            import xml.etree.ElementTree as etree
-        except ImportError:
-            try:
-                # normal cElementTree install
-                import cElementTree as etree
-            except ImportError:
-                try:
-                    # normal ElementTree install
-                    import elementtree.ElementTree as etree
-                except ImportError:
-                    pass
+from lxml import etree
 
 def str_DevFailed(df):
     """Returns a string representation of a :class:`PyTango.DevFailed`.
