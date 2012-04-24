2ae955beb201f4eda5b2d5f76bf8774ac24f9a24
tiagocoutinho <tiagocoutinho@users.sourceforge.net>
2012-04-24 16:54:37 +0000

    c
    git-svn-id: http://svn.code.sf.net/p/tango-cs/code/gui/taurus/trunk@20097 4e9c00fd-8f2e-0410-aa12-93ce3db5e235

--- lib/taurus/qt/qtgui/panel/taurusvalue.py
+++ lib/taurus/qt/qtgui/panel/taurusvalue.py
@@ -205,9 +205,13 @@ class TaurusStatusLabel(TaurusLabel):
 
 class TaurusValue(Qt.QWidget, TaurusBaseWidget):
     '''
-    .. warning:: :class:`TaurusValue` (and any derived class from it) should never be instantiated directly. 
-                 It is designed to be instantiated by a :class:`TaurusForm` class, since it
-                 breaks some conventions on the way it manages layouts of its parent model.
+    Internal TaurusValue class
+    
+    .. warning::
+    
+        :class:`TaurusValue` (and any derived class from it) should never be instantiated directly. 
+        It is designed to be instantiated by a :class:`TaurusForm` class, since it
+        breaks some conventions on the way it manages layouts of its parent model.
     '''
     __pyqtSignals__ = ("modelChanged(const QString &)",)
     
