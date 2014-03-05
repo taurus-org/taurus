6505bc688d65743a161fb7198e6f8305fded6dc3
marc rosanes <mrosanes@cells.es>
2014-03-05 18:13:30 +0100

    New features in LsTest base class
    Specific classes are now lighter because most of the
    complexity is implemented in the base class LsTest.

--- src/sardana/macroserver/macros/test/test_list.py
+++ src/sardana/macroserver/macros/test/test_list.py
@@ -34,19 +34,38 @@ from sardemoparsing import SarDemoParsing
 class LsTest(RunMacroTestCase):
     """Base class for testing macros used to list elements.
     """
+
+    header_rows = 2
+    names_column_index = 0
+    elem_type = None
+    sar_demo = SarDemoParsing()
+ 
     def setUp(self):    
         RunMacroTestCase.setUp(self)
+        
+    def check_elements(self, sardemolist, outputlist):
+        for i in sardemolist:
+            msg = "{0} does not contain {1}".format(self.macro_name, i)
+            self.assertTrue(i in outputlist, msg)
 
     def test_Run(self):
         RunMacroTestCase.test_Run(self)
         self.logOutput = self.macro_executor.getLog("output")
         msg = "generic ls macro does not contain elements"
         self.assertTrue(len(self.logOutput) > 0, msg)
-    
-    def check_elements(self, sardemolist, outputlist):
-        for i in sardemolist:
-            msg = "{0} does not contain {1}".format(self.macro_name, i)
-            self.assertTrue(i in outputlist, msg)
+
+        if (self.elem_type is not None):
+            list_sardemo = self.sar_demo.getElements(self.elem_type)
+        else:
+            raise Exception("element_type cannot be None") 
+                
+        #parsing output to get all elements
+        macro_output = []
+        for row in self.logOutput[self.header_rows:]:
+            macro_output.append(row[0].split()[self.names_column_index])
+        self.check_elements(list_sardemo, macro_output)
+
+
 
 
 class LsmTest(LsTest, unittest.TestCase):
@@ -55,22 +74,8 @@ class LsmTest(LsTest, unittest.TestCase):
        execution of the macro 'lsm'.
     """
     macro_name = "lsm"
-    sar_demo = SarDemoParsing()
+    elem_type = "moveable"
 
-    def setUp(self):
-        LsTest.setUp(self)
-        self.motorlist = self.sar_demo.getMotors() + \
-                            self.sar_demo.getPseudoMotors()
-
-    def tearDown(self):
-        self.macro_executor.unregisterAll()
-
-    def test_Run(self):      
-        LsTest.test_Run(self)
-        output_ml = []
-        for i in self.logOutput[2:]:
-            output_ml.append(i[0].split()[0])
-        self.check_elements(self.motorlist, output_ml)
 
 class LspmTest(LsTest, unittest.TestCase):
     """Class used for testing the 'lspm' macro.
@@ -78,36 +83,52 @@ class LspmTest(LsTest, unittest.TestCase):
        execution of the macro 'lspm'.
     """
     macro_name = "lspm"
-    sar_demo = SarDemoParsing()
+    elem_type = "pseudomotor"
 
-    def setUp(self):
-        LsTest.setUp(self)
-        self.pseudomotorlist = self.sar_demo.getPseudoMotors()
-
-    def test_Run(self):
-        LsTest.test_Run(self)
-        #parsing output to get all pseudomotors
-        output_pmlist = []
-        for i in self.logOutput[2:]:
-            output_pmlist.append(i[0].split()[0])
-        self.check_elements(self.pseudomotorlist, output_pmlist)
 
 class LsctrlTest(LsTest, unittest.TestCase):
-    """Class used for testing the 'lsctrl' macro.
+    """Class used for testing the 'lspm' macro.
        It verifies that all controllers created by sar_demo are listed after 
        execution of the macro 'lsctrl'.
     """
     macro_name = "lsctrl"
-    sar_demo = SarDemoParsing()
+    elem_type = "controller"
 
-    def setUp(self):
-        LsTest.setUp(self)
-        self.controllerlist = self.sar_demo.getControllers()
 
-    def test_Run(self):
-        LsTest.test_Run(self)
-        #parsing output to get all controllers
-        output_ctrllist = []
-        for i in self.logOutput[2:]:
-            output_ctrllist.append(i[0].split()[0])
-        self.check_elements(self.controllerlist, output_ctrllist)
+class LsctTest(LsTest, unittest.TestCase):
+    """Class used for testing the 'lspm' macro.
+       It verifies that all ct created by sar_demo are listed after 
+       execution of the macro 'lsct'.
+    """
+    macro_name = "lsct"
+    elem_type = "ctexpchannel"
+
+
+class Ls0dTest(LsTest, unittest.TestCase):
+    """Class used for testing the 'lspm' macro.
+       It verifies that all 0d created by sar_demo are listed after 
+       execution of the macro 'ls0d'.
+    """
+    macro_name = "ls0d"
+    elem_type = "zerodexpchannel"
+
+
+class Ls1dTest(LsTest, unittest.TestCase):
+    """Class used for testing the 'lspm' macro.
+       It verifies that all 1d created by sar_demo are listed after 
+       execution of the macro 'ls1d'.
+    """
+    macro_name = "ls1d"
+    elem_type = "onedexpchannel"
+
+
+class Ls2dTest(LsTest, unittest.TestCase):
+    """Class used for testing the 'lspm' macro.
+       It verifies that all 2d created by sar_demo are listed after 
+       execution of the macro 'ls2d'.
+    """
+    macro_name = "ls2d"
+    elem_type = "twodexpchannel"
+
+
+
