ebd9d5d531f60ea6040b5749c145720418475105
Gabriel Jover-Manas <gjover@cells.es>
2014-03-10 12:39:05 +0100

    aNscan inheritance structure created (but empty)
    The inheritance structure of scan.py has been replicated for the test.
    They are empty since they do not implement any assert.

--- src/sardana/macroserver/macros/test/test_scan.py
+++ src/sardana/macroserver/macros/test/test_scan.py
@@ -33,43 +33,61 @@ from sardana.macroserver.macros.test import macroTestRun
 from sardana.macroserver.macros.test import macroTestStop
 from sardana.macroserver.macros.test.sardemoenv import SarDemoEnv
 
+#Mockup tool
+def parsingOutput(logOutput):
+    firstdataline = 1
+    scan_index = 0
+    data = []
+    for line, in logOutput[firstdataline:]:
+        # Get a list of elements without white spaces between them
+        l = line.split() 
+        # Cast all elements of the scan line (l) to float
+        l = [float(scan_elem) for scan_elem in l]
+        # Cast index of scan to int (the first element of the list)
+        l[scan_index]= int(l[scan_index])
+        data.append(l)
+    return data 
 
-class ScanTest(RunStopMacroTestCase):
 
-    motor_list = SarDemoEnv().getMotors()
-    data = []    
+class ANscanTest(RunStopMacroTestCase):
 
     def setUp(self):
         RunStopMacroTestCase.setUp(self)
 
-        self.motor = self.macro_params[0]
-        self.initPos = float(self.macro_params[1])
-        self.finalPos = float(self.macro_params[2])
-        self.steps = int(self.macro_params[3])
-        self.dt = float(self.macro_params[4])
+    def _test_run(self):
+        RunStopMacroTestCase._test_run(self)
 
-    def test_Run(self):
-        RunStopMacroTestCase.test_Run(self)
+    def tearDown(self):
+        RunStopMacroTestCase.tearDown(self)
 
-        logOutput = self.macro_executor.getLog('output')
-        #import pdb; pdb.set_trace()
-        # loginfo - 1 not counting titles
-        # steps + 1 is the number of data points
-        self.assertEqual(len(logOutput) - 1, self.steps + 1,
-                         ("Output data lines (%d) differs " + 
-                          "from expected data points (%d)") 
-                         % (len(logOutput) - 1, self.steps + 1))
 
-        self.data = []
-        for line in logOutput[1:]:
-            l = line[0].split() 
-            l = [float(i) for i in l]
-            l[0]= int(l[0])
-            self.data.append(l)
+class DNscanTest(ANscanTest):
+
+    def setUp(self):
+        ANscanTest.setUp(self)
+
+    def _test_run(self):
+        ANscanTest._test_run(self)
+
+    def tearDown(self):
+        ANscanTest.tearDown(self)
+
+
+class DNscancTest(DNscanTest):
+
+    def setUp(self):
+        DNscanTest.setUp(self)
+
+    def _test_run(self):
+        DNscanTest._test_run(self)
+
+    def tearDown(self):
+        DNscanTest.tearDown(self)
+
 
 @macroTest('run',[SarDemoEnv().getMotors()[0], '0', '100', '4', '.1'])
 @macroTest('stop',[SarDemoEnv().getMotors()[0], '0', '100', '4', '.1'])
-class AscanTest(ScanTest, unittest.TestCase):
+class AscanTest(ANscanTest, unittest.TestCase):
     macro_name = 'ascan'
 
     def _test_run(self):
@@ -79,7 +97,10 @@ class AscanTest(ScanTest, unittest.TestCase):
         #ascan
         initPos = float(self.macro_params[1])
         finalPos = float(self.macro_params[2])
+        self.steps = int(self.macro_params[-2])
 
+        logOutput = self.macro_executor.getLog('output')
+        self.data = parsingOutput(logOutput)
         interval = abs(finalPos - initPos) / self.steps
         pre = self.data[0]
         for d in self.data[1:]:
@@ -114,30 +135,10 @@ class AscanTest(ScanTest, unittest.TestCase):
         pass  
 
 
-class AscanTest(ScanTest, unittest.TestCase):
-    macro_name = 'ascan'
-    #TODO use generator to get a arbitrary motor from sar_demo
-    macro_params = [ScanTest.motor_list[0], '0', '1', '10', '.1']
-    run_timeout = 3.
-
-    def test_Run(self):
-        ScanTest.test_Run(self)
-        self.assertEqual(self.data[0][1], self.initPos,
-                         "Initial possition differs from set value")
-        self.assertEqual(self.data[-1][1], self.finalPos,
-                         "Final possition differs from set value")
-
 @macroTestRun([SarDemoEnv().getMotors()[0], '-10', '10', '2', '.1'])
 @macroTestStop([SarDemoEnv().getMotors()[0], '-10', '10', '3', '.1'])
-class DscanTest(ScanTest, unittest.TestCase):
+class DscanTest(DNscanTest, unittest.TestCase):
     macro_name = 'dscan'
-
-    macro_params = [ScanTest.motor_list[0], '0', '1', '10', '.1']
     run_timeout = 3.
 
-    def test_Run(self):
-        ScanTest.test_Run(self)
-        #self.assertAlmostEqual(self.data[0][1] - self.data[-1][1],
-        #                       self.initPos - self.finalPos,
-        #                       "")
 
