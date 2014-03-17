e2cfe0a53ce82ef52ad78beb128ea67c35dd9e2c
Zbigniew Reszela <zreszela@cells.es>
2014-03-17 17:11:37 +0100

    Add feat-143: macro_data when macro has finished
    Macro data becomes available even after macro execution.
    This default behavior can be disabled by "PreserveMacroData"
    environment variable (macro, door and global level).
    
    MacroServerException gets raised when one tries to get macro data
    and the preserve was disabled, or no macro was run yet.

--- src/sardana/macroserver/msdoor.py
+++ src/sardana/macroserver/msdoor.py
@@ -39,6 +39,7 @@ from sardana.sardanaevent import EventType
 
 from sardana.macroserver.msbase import MSObject
 from sardana.macroserver.msparameter import Type
+from sardana.macroserver.msexception import MacroServerException
 
 
 class MacroProxy(object):
@@ -131,7 +132,8 @@ class MSDoor(MSObject):
     def get_macro_data(self):
         macro = self.running_macro
         if macro is None:
-            raise Exception("No macro has run so far!")
+            raise MacroServerException("No macro has run so far " + \
+                            "or the macro data was not preserved.")
         data = macro.data
         return data
 
