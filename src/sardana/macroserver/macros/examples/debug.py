2e172401288ccfb6f437e99eda4a0afdd569bcec
cpascual <cpascual@cells.es>
2014-03-20 13:48:31 +0100

    Add raise_exception to macro examples
    Add a new macro to the examples which raises an exception when called.
    It is useful for debugging.

---
+++ src/sardana/macroserver/macros/examples/debug.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+#############################################################################
+##
+## This file is part of Sardana, a Tango User Interface Library
+## 
+## http://www.tango-controls.org/static/taurus/latest/doc/html/index.html
+##
+## Copyright 2011 CELLS / ALBA Synchrotron, Bellaterra, Spain
+## 
+## Sardana is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Lesser General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+## 
+## Sardana is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+## 
+## You should have received a copy of the GNU Lesser General Public License
+## along with Sardana.  If not, see <http://www.gnu.org/licenses/>.
+##
+#############################################################################
+
+"""This module provides ..."""
+
+#__all__ = []
+
+__docformat__ = 'restructuredtext'
+
+from sardana.macroserver.macro import Type, macro
+
+
+@macro()
+def raise_exception(self):
+    '''A macro that raises an exception (useful for debugging)'''
+    raise Exception('here is your exception!') 
