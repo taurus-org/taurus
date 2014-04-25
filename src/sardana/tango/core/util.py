96fe6ae346b2ee2f2a144cc869e7157fe39e7c7b
zreszela <zreszela@cells.es>
2014-04-25 19:11:53 +0200

    Fix bug-156: fix get_free_alias
    "get_free_alias" tries to retrieve device from its alias,
    when fails it means that the alias is not in use yet.
    
    "get_device_from_alias" is used from PyTango 8.1.0 Database API.
    Fallback uses "get_device_alias" for previous PyTango versions.

--- src/sardana/tango/core/util.py
+++ src/sardana/tango/core/util.py
@@ -750,13 +750,26 @@ def get_free_device(db, prefix, start_from=1):
     return prefix + "/" + str(start_from)
 
 def get_free_alias(db, prefix, start_from=1):
+    '''Iterates until failure, trying to retrieve from the database device of
+    the given alias. This way, first which fails is available in the database.
+
+    :param db: database where to look for the free alias
+    :type db: PyTango.Database
+    :param start_from: alias suffix in form of consecutive number
+    :type start_from: int
+    '''
     while True:
         name = prefix + "_" + str(start_from)
         try:
-            db.get_alias(name)
-            start_from += 1
-        except:
+            db.get_device_from_alias(name) # PyTango >= 8.1.0
+        except PyTango.DevFailed:
             return name
+        except AttributeError:
+            try:
+                db.get_device_alias(name) # deprecated since PyTango 8.1.0
+            except PyTango.DevFailed:
+                return name
+        start_from += 1
 
 def prepare_taurus(options, args, tango_args):
     # make sure the polling is not active
