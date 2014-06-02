ca3749c5574633164247e0f192f1569701509c5c
zreszela <zreszela@cells.es>
2014-06-02 17:47:54 +0200

    Fix documentation errors
    Proper use of reference to function object.

--- src/sardana/macroserver/macros/test/base.py
+++ src/sardana/macroserver/macros/test/base.py
@@ -42,7 +42,7 @@ _NOT_PASSED = __NotPassedType()
 
 def macroTest(klass=None, helper_name=None, test_method_name=None,
               test_method_doc=None, **helper_kwargs):
-    """This decorator is an specialization of :function:`taurus.test.insertTest` 
+    """This decorator is an specialization of :function::`taurus.test.insertTest`
     for macro testing. It inserts test methods from a helper method that may 
     accept arguments.
 
@@ -109,7 +109,7 @@ def macroTest(klass=None, helper_name=None, test_method_name=None,
         class FooTest(RunMacroTestCase, unittest.TestCase):
             macro_name = 'twice'
             
-    .. seealso:: :function:`taurus.test.insertTest`
+    .. seealso:: :function::`taurus.test.insertTest`
 
     """
     #recipe to allow decorating with and without arguments
