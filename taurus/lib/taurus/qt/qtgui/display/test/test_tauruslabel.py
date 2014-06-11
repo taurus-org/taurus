9709da026602780b10a5dac00c162541a1b74e09
cpascual <cpascual@cells.es>
2014-06-11 10:43:30 +0200

    Add test for bug-169

--- taurus/lib/taurus/qt/qtgui/display/test/test_tauruslabel.py
+++ taurus/lib/taurus/qt/qtgui/display/test/test_tauruslabel.py
@@ -26,8 +26,9 @@
 """Unit tests for Taurus Label"""
 
 from taurus.external import unittest
-from taurus.qt.qtgui.test import GenericWidgetTestCase
+from taurus.qt.qtgui.test import GenericWidgetTestCase, BaseWidgetTestCase
 from taurus.qt.qtgui.display import TaurusLabel
+from taurus.qt.qtgui.container import TaurusWidget
 
 
 class TaurusLabelTest(GenericWidgetTestCase, unittest.TestCase):
@@ -39,6 +40,45 @@ class TaurusLabelTest(GenericWidgetTestCase, unittest.TestCase):
     '''
     _klass = TaurusLabel
     modelnames = ['sys/tg_test/1/wave', '', 'eval://1', None]
+    
+class Bug169_Test(BaseWidgetTestCase, unittest.TestCase):
+
+    '''
+    Test bug169: 
+    
+        AttributeError: type object 'TaurusConfigurationProxy' has no attribute
+        'buildModelName'.
+        
+        See: http://sf.net/p/sardana/tickets/169/
+    
+
+    .. seealso: :class:`taurus.qt.qtgui.test.base.BaseWidgetTestCase`
+    '''
+    _klass = TaurusLabel
+    
+    def setUp(self):
+        BaseWidgetTestCase.setUp(self)
+        self._widget.setModel('sys/tg_test/1/double_scalar?configuration=label')
+        self._expectedModelClass = self._widget.getModelClass()
+        self._parent = TaurusWidget()
+        self._parent.setModel('sys/tg_test/1')
+        self._widget.setUseParentModel(True)
+        self._widget.setParent(self._parent)
+        
+    def test_bug169(self):
+        '''Check if setModel works when using parent model''' 
+        self._widget.setModel('/double_scalar?configuration=label')
+        
+    def test_relativemodelclass(self):
+        '''Check consistency in modelClass when using parent model (re: bug169)
+        ''' 
+        try:
+            self._widget.setModel('/double_scalar?configuration=label')
+        finally:
+            mc = self._widget.getModelClass()
+            msg = ('getModelClass() inconsistency:\n expected: %s\n got: %s' % 
+                   (self._expectedModelClass, mc))
+            self.assertEqual(self._expectedModelClass, mc, msg)
 
 
 #
