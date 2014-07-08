d786e4c1632877cf8217c57fe5c0656d96df24d8
cpascual <cpascual@cells.es>
2014-07-08 13:33:56 +0200

    Bumped Taurus version number to 3.3

--- taurus/lib/taurus/core/release.py
+++ taurus/lib/taurus/core/release.py
@@ -53,7 +53,7 @@ name = 'taurus'
 # bdist_deb does not accept underscores (a Debian convention).
 
 
-version_info = (3,2,0,'rc',0)
+version_info = (3,3,0,'rc',0)
 version = '.'.join(map(str, version_info[:3]))
 revision = str(version_info[4])
 
