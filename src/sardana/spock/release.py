42c824eac614d5e98d7398de090f8d68bebd2ede
zreszela <zreszela@cells.es>
2014-07-30 16:42:18 +0200

    Bump spock version number
    Previously the spock application had its own version number, however there
    is no reason to not follow the Sardana version number.

--- src/sardana/spock/release.py
+++ src/sardana/spock/release.py
@@ -39,7 +39,7 @@ name = 'spock'
 revision = '1'
 
 #version = '0.8.1.svn.r' + revision.rstrip('M')
-version = '1.0.0'
+version = '1.4.0'
 
 description = "An enhanced interactive Macro Server shell."
 
