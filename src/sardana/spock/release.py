b192b5562685c0abf624e3248626b2d4c8bf001d
zreszela <zreszela@cells.es>
2014-10-27 09:47:23 +0100

    Bump version numbers (sardana & spock -> 1.4.2)

--- src/sardana/spock/release.py
+++ src/sardana/spock/release.py
@@ -39,7 +39,7 @@ name = 'spock'
 revision = '1'
 
 #version = '0.8.1.svn.r' + revision.rstrip('M')
-version = '1.4.1'
+version = '1.4.2'
 
 description = "An enhanced interactive Macro Server shell."
 
