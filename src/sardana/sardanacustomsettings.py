742102b5f63ea29c9615fa6872ac664f60599262
Marc Josep Rosanes Siscart <mrosanes@cells.es>
2015-01-21 11:32:42 +0100

    Let user choose the Pool used by tests
    Add variables in sardanacustomsettings to allow user control over 
    the Pool DS and Pool device names to be used by Sardana unittests.

--- src/sardana/sardanacustomsettings.py
+++ src/sardana/sardanacustomsettings.py
@@ -29,10 +29,13 @@ The idea is that the final user may edit the values here to customize certain
 aspects of Sardana.
 """
 
-
 #:UnitTest door name: the door to be used by unit tests.
 #:UNITTEST_DOOR_NAME Must be defined for running sardana unittests. 
 UNITTEST_DOOR_NAME = "door/demo1/1"
+#:UnitTests Pool DS name: Pool DS to use in unit tests.
+UNITTEST_POOL_DS_NAME = "unittest1"
+#:UnitTests Pool Device name: Pool Device to use in unit tests.
+UNITTEST_POOL_NAME = "pool/demo1/1"
 
 #:Size and number of rotating backups of the log files.
 #:The Pool and MacroServer Device servers will use these values for their logs. 
