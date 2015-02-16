9fe86d679e0af136c20790944f8eab5c3bef3c80
cpascual <cpascual@cells.es>
2015-02-16 11:02:15 +0100

    Update URL in licence header for sardana
    Latest change in Tango web page broke the project URL in the licence
    notices. Update it to http://www.sardana-controls.org

--- src/sardana/tango/__init__.py
+++ src/sardana/tango/__init__.py
@@ -4,7 +4,7 @@
 ##
 ## This file is part of Sardana
 ##
-## http://www.tango-controls.org/static/sardana/latest/doc/html/index.html
+## http://www.sardana-controls.org/
 ##
 ## Copyright 2011 CELLS / ALBA Synchrotron, Bellaterra, Spain
 ## 
