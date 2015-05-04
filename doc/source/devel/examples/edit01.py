04f89581639d5f74057ed6bf32c16d2b57d25659
cpascual <cpascual@cells.es>
2015-05-04 16:25:46 +0200

    Adapt examples to changes in configuration syntax
    Taurus TEP3 changes the syntax for configuration model names from using
    ?configuration[=<cfg_key>] to #[<cfg_key>]. Adapt example scripts in
    devel docs to that.

--- doc/source/devel/examples/edit01.py
+++ doc/source/devel/examples/edit01.py
@@ -18,10 +18,10 @@ layout.addWidget(w1)
 layout.addWidget(w2)
 layout.addWidget(w3)
 layout.addWidget(w4)
-w1.model, w1.bgRole = 'sys/tg_test/1/double_scalar?configuration=label', ''
+w1.model, w1.bgRole = 'sys/tg_test/1/double_scalar#label', ''
 w2.model = 'sys/tg_test/1/double_scalar'
 w3.model = 'sys/tg_test/1/double_scalar'
-w4.model, w4.bgRole = 'sys/tg_test/1/double_scalar?configuration=unit', ''
+w4.model, w4.bgRole = 'sys/tg_test/1/double_scalar#unit', ''
 
 panel.show()
 sys.exit(app.exec_())
