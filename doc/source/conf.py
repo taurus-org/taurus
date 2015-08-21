0efc0c9d53d0508c785398b6e79c2ae05a1ec582
zreszela <zreszela@cells.es>
2015-08-21 09:48:43 +0200

    Use sidebar logo instead of custom template
    Use official logo in the sidebar instead of customizing the template and
    placing logo on top of the page.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -212,7 +212,8 @@ html_theme_path = []
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+# using bmp, cause the logo's width should not exceed 200 pixels
+html_logo = os.path.join(os.pardir, os.pardir, 'taurus.bmp')
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
