82eaea8cfa60c68513d477a2c96f721b8d9c40b7
cpascual <cpascual@cells.es>
2015-12-02 13:47:01 +0100

    Fix modelequality tests (partial fix for bug-233)
    Refactor the modelequality test base class (poor error messaging
    and docsting) and change the model names used in the tango
    model equality tests to avoid exceptions due to non-existing
    devices

--- lib/taurus/core/tango/test/test_modelequality.py
+++ lib/taurus/core/tango/test/test_modelequality.py
@@ -8,42 +8,42 @@ dev_models1 = ['tango:sys/tg_test/1',
                'tango:SYS/TG_TEST/1']
 # different device models
 dev_models2 = ['tango:sys/tg_test/1',
-               'tango:sys/tg_test/2']
+               'tango:sys/database/2']
 # the same attribute models, just different cases
 attr_models1 = ['tango:sys/tg_test/1/float_scalar',
                 'tango:SYS/TG_TEST/1/FLOAT_SCALAR']
-# different attribute models
-attr_models2 = ['tango:sys/tg_test/1/float_scalar',
-                'tango:sys/tg_test/2/float_scalar']
-# the same configuration models, just different cases
-conf_models1 = [
+# different attribute models (same attr from different device)
+attr_models2 = ['tango:sys/tg_test/1/state',
+                'tango:sys/database/2/state']
+# the same attribute models, just different cases
+attr_models3 = [
         'tango:sys/tg_test/1/float_scalar#label',
         'tango:SYS/TG_TEST/1/FLOAT_SCALAR#LABEL'
         ]
-# different configuration models
-conf_models2 = [
-        'tango:sys/tg_test/1/float_scalar#label',
-        'tango:sys/tg_test/2/float_scalar#label'
+# different attribute models (same attr and fragment but different device)
+attr_models4 = [
+        'tango:sys/tg_test/1/state#label',
+        'tango:sys/database/2/state#label'
         ]
-# the same configuration models, just different cfgkeys
-conf_models3 = [
+# the same attribute models, just different fragment
+attr_models5 = [
         'tango:sys/tg_test/1/float_scalar#label',
         'tango:SYS/TG_TEST/1/FLOAT_SCALAR#range'
         ]
 
-# test cases for the model case insensitiveness
+# # test cases for the model case insensitiveness
 @testAttributeModelEquality(models = attr_models1, equal = True)
 @testDeviceModelEquality(models = dev_models1, equal = True)
-@testAttributeModelEquality(models = conf_models1, equal = True)
+@testAttributeModelEquality(models = attr_models3, equal = True)
 
 # test cases for the different models inequality
 @testAttributeModelEquality(models = attr_models2, equal = False)
 @testDeviceModelEquality(models = dev_models2, equal = False)
-@testAttributeModelEquality(models = conf_models2, equal = False)
+@testAttributeModelEquality(models = attr_models4, equal = False)
 
 # test cases for the different models equality
 #TODO: add device/alias equality tests
-@testAttributeModelEquality(models = conf_models3, equal = True)
+@testAttributeModelEquality(models = attr_models5, equal = True)
 class TangoModelEqualityTestCase(TaurusModelEqualityTestCase, 
                                  unittest.TestCase):
     """TestCase class for tango model equality testing."""
--- lib/taurus/core/test/modelequality.py
+++ lib/taurus/core/test/modelequality.py
@@ -35,29 +35,21 @@ class TaurusModelEqualityTestCase(object):
         of taurus objects e.g. TaurusAttribute.
 
         :param models: (seq<str>): a sequence of two taurus models
-        :param class_: (type) a class (or a factory function) that will be used
-                       to create objects
-        :param equal: (bool) a flag argument, whether to check equality or 
-                      inequality
+        :param class_: (function) model factory function
+        :param equal: (bool) If True, check equality. Else check inequality
         """
-
-        if len(models) > 2:
-            raise ValueError('models length is higher than 2')        
-        obj1 = class_(models[0])        
-        obj2 = class_(models[1])        
+        name1, name2 = models
+        obj1 = class_(name1)
+        obj2 = class_(name2)
         if equal:
-            msg = '%s objects: %s and %s are not equal but should be' % \
-                  (class_.__name__, obj1, obj2)
-            self.assertEqual(obj1, obj2, msg)            
+            msg = 'models for %s and %s are not equal (they should)' % \
+                  (name1, name2)
+            self.assertIs(obj1, obj2, msg)
         else:
-            msg = '%s objects: %s and %s are equal but should not be' % \
-                  (class_.__name__, obj1, obj2)
-            self.assertNotEqual(obj1, obj2, 'Case insensitivity failed')
-            
-# testAuthorityModelEquality = functools.partial(insertTest, 
-#                               helper_name = 'modelsSensitive',
-#                               class_ = Authority,
-#                               test_method_name = 'testAuthorityModelEquality')
+            msg = 'models for %s and %s are equal (they should not)' % \
+                  (name1, name2)
+            self.assertIsNot(obj1, obj2, msg)
+
             
 testDeviceModelEquality = functools.partial(insertTest, 
                                    helper_name = 'modelsEqual',
@@ -68,61 +60,3 @@ testAttributeModelEquality = functools.partial(insertTest,
                                 helper_name = 'modelsEqual',
                                 class_ = Attribute,
                                 test_method_name = 'testAttributeModelEquality')
-
-# def set_alias(device):
-#     import PyTango
-#     db = PyTango.Database()
-#     db.put_device_alias(device, 'unit_test')
-#     return 'unit_test'
-# 
-# def get_alias(device):
-#     import PyTango
-#     d = PyTango.DeviceProxy(device)
-#     try:
-#         return d.alias()
-#     except:
-#         return None
-# 
-# def del_alias():
-#     import PyTango
-#     db = PyTango.Database()
-#     db.delete_device_alias('unit_test')        
-#         
-# class TaurusCaseSensitivityTest(unittest.TestCase):
-# 
-#     def setUp(self):
-#         self.tango_device_name = ["sys/tg_test/1"]
-# 
-#     def test_tango_device(self):
-#         '''Test tango case insensitivity'''
-#         for d in self.tango_device_name:
-#             delalias = False
-#             alias = get_alias(d)
-#             if alias is None:
-#                 alias = set_alias(d)
-#                 delalias = True
-#             self.check_tango_device(d,alias)    
-#             if delalias:
-#                 del_alias()
-# 
-#     def check_tango_device(self, device_name, device_alias):
-#         tango_host = os.environ['TANGO_HOST']
-#         tango_scheme = "tango://"
-#             
-#         m1 = device_name
-#         m2 = tango_scheme + tango_host + '/' + rand_uppercase(m1) 
-#         m3 = device_alias
-#         m4 = tango_scheme + rand_uppercase(m3)
-#         ### Tango devices
-#         d1 = Device(rand_uppercase(m1))
-#         d2 = Device(m2)
-#         d3 = Device(rand_uppercase(m3))
-#         d4 = Device(m4)
-# 
-# 
-#         self.assertEqual(d1, d2, "Device, %s is not equal %s" %(d1,d2))
-#         self.assertEqual(d3, d4, "Device, %s is not equal %s" %(d3,d4))
-#         self.assertEqual(d1, d3, "Device, %s is not equal %s" %(d1,d2))
-#         
-# if __name__ == '__main__':    
-#     unittest.main(verbosity=2)
