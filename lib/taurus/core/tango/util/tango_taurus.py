c71ce120133e2d2a5888f343b75ce113db2875a8
cpascual <cpascual@cells.es>
2017-03-22 10:01:25 +0100

    Deprecate unit_from_tango
    The unit_from_tango method is no longer used (its functionality was
    moved to TangoAttribute._unit_from_tango).
    Deprecate it to avoid maintaining duplicated code.

--- lib/taurus/core/tango/util/tango_taurus.py
+++ lib/taurus/core/tango/util/tango_taurus.py
@@ -114,6 +114,9 @@ def quantity_from_tango_str(value_str, dtype=None, units=None, fmt=None,
 
 
 def unit_from_tango(unit):
+    from taurus import deprecated
+    deprecated(dep='unit_from_tango', rel='4.0.4', alt="pint's parse_units")
+
     if unit == PyTango.constants.UnitNotSpec:
         unit = None
     try:
