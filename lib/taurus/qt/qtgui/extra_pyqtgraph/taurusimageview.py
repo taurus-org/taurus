8dec4e3bd18b0b5f7d85d76d24a4588e56df1786
oscarPrades <oprades@axt.email>
2017-03-24 10:25:01 +0100

    Implements data and image plot with pyqtgraph.
    PyQwt5 is deprecated. Add a first proof of concept for a
    'Taurus-ified' extension based in pyqtgraph. Add plotting for data
    and 2D and 3D image data.

---
+++ lib/taurus/qt/qtgui/extra_pyqtgraph/taurusimageview.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+#############################################################################
+##
+# This file is part of Taurus
+##
+# http://taurus-scada.org
+##
+# Copyright 2011 CELLS / ALBA Synchrotron, Bellaterra, Spain
+##
+# Taurus is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+##
+# Taurus is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+##
+# You should have received a copy of the GNU Lesser General Public License
+# along with Taurus.  If not, see <http://www.gnu.org/licenses/>.
+##
+#############################################################################
+
+import sys
+from taurus.qt.qtgui.application import TaurusApplication
+from taurus.qt.qtgui.base import TaurusBaseComponent
+from pyqtgraph import ImageView
+import numpy as np
+
+
+class TaurusImageView(ImageView, TaurusBaseComponent):
+    """
+    Displays 2D and 3D image data
+    """
+    def __init__(self, *args, **kwargs):
+        ImageView.__init__(self, *args, **kwargs)
+        TaurusBaseComponent.__init__(self, 'TaurusImageView')
+
+    def handleEvent(self, evt_src, evt_type, evt_val):
+        try:
+            data = evt_val.rvalue
+            self.setImage(data)
+        except Exception, e:
+            self.warning('Exception in handleEvent: %s', e)
+
+
+if __name__ == "__main__":
+    import pyqtgraph as pg
+
+    app = TaurusApplication()
+
+    axis_view = pg.PlotItem()
+
+    #set param AxisView for display the axis from the plot view
+    imv = TaurusImageView(view=axis_view)
+
+    #set a custom color map
+    colors = [
+        (0, 0, 0),
+        (45, 5, 61),
+        (84, 42, 55),
+        (150, 87, 60),
+        (208, 171, 141),
+        (230, 230, 230)
+    ]
+    cmap = pg.ColorMap(pos=np.linspace(0.0, 1.0, 6), color=colors)
+    imv.setColorMap(cmap)
+
+    # Add random 3D image data
+    # imv.setImage(np.random.normal(size=(100,200,200)))
+
+    #Add taurus 2D image data
+    imv.setModel('eval:rand(256,256)')
+
+    imv.show()
+    sys.exit(app.exec_())
---
+++ lib/taurus/qt/qtgui/extra_pyqtgraph/taurusplotdataitem.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+#############################################################################
+##
+# This file is part of Taurus
+##
+# http://taurus-scada.org
+##
+# Copyright 2011 CELLS / ALBA Synchrotron, Bellaterra, Spain
+##
+# Taurus is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+##
+# Taurus is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+##
+# You should have received a copy of the GNU Lesser General Public License
+# along with Taurus.  If not, see <http://www.gnu.org/licenses/>.
+##
+#############################################################################
+
+
+import sys
+import numpy
+from taurus.qt.qtgui.application import TaurusApplication
+from taurus.qt.qtgui.base import TaurusBaseComponent
+from pyqtgraph import PlotDataItem
+
+class TaurusPlotDataItem(PlotDataItem, TaurusBaseComponent):
+    """A taurus-ified PlotDataItem"""
+
+    def __init__(self, *args, **kwargs):
+        PlotDataItem.__init__(self, *args, **kwargs)
+        TaurusBaseComponent.__init__(self, 'TaurusBaseComponent')
+
+    def handleEvent(self, evt_src, evt_type, evt_value):
+        try:
+            y = evt_value.rvalue
+            self.setData(y)
+        except Exception, e:
+            self.warning('Exception in handleEvent: %s', e)
+
+
+
+if __name__ == '__main__':
+    import pyqtgraph as pg
+
+    app = TaurusApplication()
+
+    # a standard pyqtgraph widget
+    w = pg.PlotWidget()
+
+    #add legend to the plot, for that we have to give a name to plot items
+    w.addLegend()
+
+    # adding a regular data item (non-taurus)
+    c1 = pg.PlotDataItem(name='st plot',pen='b', fillLevel=0, brush='c')
+    c1.setData(numpy.arange(300) / 300.)
+    w.addItem(c1)
+
+    # adding a taurus data item
+    c2 = TaurusPlotDataItem(name='st2 plot',pen='r', symbol='o')
+    c2.setModel('eval:Quantity(rand(256),"m")')
+
+    w.addItem(c2)
+
+    w.show()
+
+    sys.exit(app.exec_())
