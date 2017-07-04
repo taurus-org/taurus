f5d7b7496796c1173f2308f476d1d677f4b990f1
oscarPrades <oprades@axt.email>
2017-07-04 11:38:42 +0200

    Implement setXmodel
    Allows to user build a TaurusPlotDataItem setting a model for
    absissas values (X axis)

--- lib/taurus/qt/qtgui/extra_pyqtgraph/taurusplotdataitem.py
+++ lib/taurus/qt/qtgui/extra_pyqtgraph/taurusplotdataitem.py
@@ -24,6 +24,7 @@
 #############################################################################
 
 from taurus import Attribute
+from taurus.core import TaurusEventType
 from taurus.qt.qtgui.base import TaurusBaseComponent
 from pyqtgraph import PlotDataItem
 
@@ -32,22 +33,44 @@ class TaurusPlotDataItem(PlotDataItem, TaurusBaseComponent):
     """A taurus-ified PlotDataItem"""
 
     def __init__(self, *args, **kwargs):
+        """
+        Accepts same args and kwargs as PlotDataItem, plus:
+        :param xModel: (str) Taurus model name for abscissas values.
+                       Default=None
+        :param yModel: (str) Taurus model name for ordinate values.
+                       Default=None
+        """
+        xModel = kwargs.pop('xModel', None)
+        yModel = kwargs.pop('yModel', None)
         PlotDataItem.__init__(self, *args, **kwargs)
         TaurusBaseComponent.__init__(self, 'TaurusBaseComponent')
+        self._x = None
+        self._y = None
+        self.xModel = None
+        if xModel is not None:
+            self.setXmodel(xModel)
+        if yModel is not None:
+            self.setModel(yModel)
 
-    def setXmodel(self, xmodel):
-        # TODO: Add functionallity for set x model to plotDataItem
-        self.xModel = Attribute(xmodel)
+    def setXmodel(self, xModel):
+        if xModel is None:
+            if self.xModel is not None:
+                self.xModel.removeListener(self)
+            self.xModel = None
+            return
+        self.xModel = Attribute(xModel)
         self._x = self.xModel.read().rvalue
-        # self.xModel.addListener(self)
+        self.xModel.addListener(self)
 
     def handleEvent(self, evt_src, evt_type, evt_value):
-        try:
-            y = evt_value.rvalue
-            self.setData(y)
-        except Exception, e:
-            self.warning('Exception in handleEvent: %s', e)
-
+        if evt_type not in (TaurusEventType.Change, TaurusEventType.Periodic):
+            return
+        yModel = self.getModelObj()
+        if yModel == evt_src and yModel is not None:
+            self._y = evt_value.rvalue
+        if self.xModel == evt_src and self.xModel is not None:
+            self._x = evt_value.rvalue
+        self.setData(x=self._x, y=self._y)
 
 
 if __name__ == '__main__':
@@ -66,7 +89,7 @@ if __name__ == '__main__':
     w.addLegend()
 
     # adding a regular data item (non-taurus)
-    c1 = pg.PlotDataItem(name='st plot',pen='b', fillLevel=0, brush='c')
+    c1 = pg.PlotDataItem(name='st plot', pen='b', fillLevel=0, brush='c')
     c1.setData(numpy.arange(300) / 300.)
     w.addItem(c1)
 
@@ -74,8 +97,9 @@ if __name__ == '__main__':
     c2 = TaurusPlotDataItem(name='st2 plot', pen='r', symbol='o')
     c2.setModel('eval:Quantity(rand(256),"m")')
 
+    # c2.setXmodel('eval:Quantity(rand(256),"m")')
+
     w.addItem(c2)
 
     w.show()
-
     sys.exit(app.exec_())
