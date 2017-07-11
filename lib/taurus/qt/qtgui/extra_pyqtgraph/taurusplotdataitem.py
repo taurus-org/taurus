a003e024310ef07323c84dd46025421a08192392
oscarPrades <oprades@axt.email>
2017-07-11 11:24:42 +0200

    Add save and restore options in TaurusPlotDataItem
    Save and restore curve properties and models contained in
    TaurusPlotDataItem.

--- lib/taurus/qt/qtgui/extra_pyqtgraph/taurusplotdataitem.py
+++ lib/taurus/qt/qtgui/extra_pyqtgraph/taurusplotdataitem.py
@@ -22,11 +22,11 @@
 # along with Taurus.  If not, see <http://www.gnu.org/licenses/>.
 ##
 #############################################################################
-
 from taurus import Attribute
 from taurus.core import TaurusEventType
 from taurus.qt.qtgui.base import TaurusBaseComponent
 from pyqtgraph import PlotDataItem
+import pyqtgraph as pg
 
 
 class TaurusPlotDataItem(PlotDataItem, TaurusBaseComponent):
@@ -52,6 +52,12 @@ class TaurusPlotDataItem(PlotDataItem, TaurusBaseComponent):
         if yModel is not None:
             self.setModel(yModel)
 
+        self.registerConfigProperty(self.getOpts, self.setOpts, 'opts')
+        self.setModelInConfig(True)
+        if self._modelInConfig:
+            self.registerConfigProperty(self.getXmodel, self.setXmodel,
+                                        'XModel')
+
     def setXmodel(self, xModel):
         if xModel is None:
             if self.xModel is not None:
@@ -62,6 +68,12 @@ class TaurusPlotDataItem(PlotDataItem, TaurusBaseComponent):
         self._x = self.xModel.read().rvalue
         self.xModel.addListener(self)
 
+    def getXmodel(self):
+        if self.xModel is None:
+            return None
+        else:
+            return self.xModel.getFullName()
+
     def handleEvent(self, evt_src, evt_type, evt_value):
         if evt_type not in (TaurusEventType.Change, TaurusEventType.Periodic):
             return
@@ -71,6 +83,109 @@ class TaurusPlotDataItem(PlotDataItem, TaurusBaseComponent):
         if self.xModel == evt_src and self.xModel is not None:
             self._x = evt_value.rvalue
         self.setData(x=self._x, y=self._y)
+
+    def getOpts(self):
+        return self.serialize(self.opts)
+
+    def setOpts(self, opts):
+        # creates QObjects from a pickle loaded file
+        # for adapt the serialized objects into PlotDataItem properties
+
+        # pen property
+        if opts['pen'] is not None:
+            opts['pen'] = self.unmarshallingQPainter(opts, 'pen', 'pen')
+
+        # shadowPen property
+        if opts['shadowPen'] is not None:
+            opts['shadowPen'] = self.unmarshallingQPainter(
+                opts, 'shadowPen', 'pen')
+
+        # symbolPen property
+        if opts['symbolPen'] is not None:
+            opts['symbolPen'] = self.unmarshallingQPainter(
+                opts, 'symbolPen', 'pen')
+
+        # fillBrush property
+        if opts['fillBrush'] is not None:
+            opts['fillBrush'] = self.unmarshallingQPainter(
+                opts, 'fillBrush', 'brush')
+
+        # symbolBrush property
+        if opts['symbolBrush'] is not None:
+            opts['symbolBrush'] = self.unmarshallingQPainter(
+                opts, 'symbolBrush', 'brush')
+
+        self.opts = opts
+        print self.opts
+
+        # we have to set connect property (maybe pyqtgraph bug?)
+        # in that case, we have to check all values: all, pairs, finite
+        if opts['connect'] == 'all':
+            self.opts['connect'] = 'all'
+        elif opts['connect'] == 'pairs':
+            self.opts['connect'] = 'pairs'
+        elif opts['connect'] == 'finite':
+            self.opts['connect'] = 'finite'
+
+    def unmarshallingQPainter(self, opts, prop_name, qPainter):
+
+        color = opts[prop_name]
+        style = opts[prop_name + '_style']
+        del opts[prop_name + '_style']
+
+        if qPainter == 'pen':
+            width = opts[prop_name + '_width']
+            del opts[prop_name + '_width']
+            painter = pg.mkPen(color=color, style=style, width=width)
+        elif qPainter == 'brush':
+            painter = pg.mkBrush(color=color)
+            painter.setStyle(style)
+        else:
+            return
+
+        return painter
+
+    def serialize(self, opts):
+        """
+        This method serialize all properties from PlotDataItem
+        :return: dict of serialized properties
+        """
+
+        # pen property (QPen object)
+        if opts['pen'] is not None:
+            self.marshallingQPainter(opts, 'pen', 'pen')
+
+        # shadowPen property (QPen object)
+        if opts['shadowPen'] is not None:
+            self.marshallingQPainter(opts, 'shadowPen', 'pen')
+
+        # symbolPen property (QPen object)
+        if opts['symbolPen'] is not None:
+            self.marshallingQPainter(opts, 'symbolPen', 'pen')
+
+        # fillBrush property (QBrush object)
+        if opts['fillBrush'] is not None:
+            self.marshallingQPainter(opts, 'fillBrush', 'brush')
+
+        # symbolBrush property (QBrush object)
+        if opts['symbolBrush'] is not None:
+            self.marshallingQPainter(opts, 'symbolBrush', 'brush')
+
+        return opts
+
+    def marshallingQPainter(self, opts, prop_name, qPainter):
+        if qPainter == 'pen':
+            painter = pg.mkPen(opts[prop_name])
+            opts[prop_name + '_width'] = painter.width()
+        elif qPainter == 'brush':
+            painter = pg.mkBrush(opts[prop_name])
+        else:
+            return
+
+        color = pg.colorStr(painter.color())
+        opts[prop_name] = color
+        opts[prop_name + '_style'] = painter.style()
+
 
 
 if __name__ == '__main__':
@@ -93,13 +208,35 @@ if __name__ == '__main__':
     c1.setData(numpy.arange(300) / 300.)
     w.addItem(c1)
 
+
+    pen = pg.mkPen(color='r', style=2)
+    brush = pg.mkBrush(color='b')
+    brush.setStyle(3)
+
     # adding a taurus data item
-    c2 = TaurusPlotDataItem(name='st2 plot', pen='r', symbol='o')
-    c2.setModel('eval:Quantity(rand(256),"m")')
+    # c2 = TaurusPlotDataItem(name='st2 plot', pen='r', symbol='o')
+    # c2 = TaurusPlotDataItem(pen='y',symbolPen=pen, symbol='o', symbolBrush=brush)
+    c2 = TaurusPlotDataItem()
+
+
+
+    c2.loadConfigFile('tmp/conf.cfg')
+
+
+    # c2.setModel('eval:Quantity(rand(256),"m")')
+    # c2.setModel('sys/tg_test/1/wave')
+    # c2.setModel(None)
+    # c2.setXmodel(None)
 
     # c2.setXmodel('eval:Quantity(rand(256),"m")')
 
     w.addItem(c2)
-
     w.show()
-    sys.exit(app.exec_())
+
+    res = app.exec_()
+
+    config = c2.createConfig()
+    # c2.saveConfigFile('tmp/conf.cfg')
+    print config
+
+    sys.exit(res)
