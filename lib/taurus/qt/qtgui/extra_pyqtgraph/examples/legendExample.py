dd9e299e4e0251029813ab939cc97b34fde873ab
oscarPrades <oprades@axt.email>
2017-07-21 10:35:34 +0200

    Example of use legend Item.
    This is an example of how to use a pyqtgraph.LegendItem in a
    pop up window outside for the main widget plot.

---
+++ lib/taurus/qt/qtgui/extra_pyqtgraph/examples/legendExample.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Demonstrates basic use of LegendItem
+
+"""
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtGui
+
+
+
+
+## Start Qt event loop unless running in interactive mode or using pyside.
+if __name__ == '__main__':
+    import sys
+
+    app = QtGui.QApplication([])
+
+    plt = pg.PlotWidget()
+    # plt = pg.plot()
+    plt.setWindowTitle('pyqtgraph example: PLOT')
+
+    plt2 = QtGui.QGraphicsView(QtGui.QGraphicsScene())
+    plt2.setWindowTitle('pyqtgraph example: Legend')
+    plt2.setBackgroundBrush(QtGui.QBrush(QtGui.QColor('black')))
+
+    l = pg.LegendItem((100,60), offset=(70,30))  # args are (size, offset)
+
+    c1 = plt.plot([1, 3, 2, 4], pen='r', symbol='o', symbolPen='r',
+                  symbolBrush=0.5, name='red plot')
+    c2 = plt.plot([2, 1, 4, 3], pen='g', fillLevel=0,
+                  fillBrush=(255, 255, 255, 30), name='green plot')
+    c3 = plt.plot(range(7), pen='c', fillLevel=0)
+
+    for dataitem in plt.getPlotItem().items:
+        if dataitem.name():
+            l.addItem(dataitem, dataitem.name())
+
+    plt2.scene().addItem(l)
+    plt.show()
+    plt2.show()
+    sys.exit(app.exec_())
