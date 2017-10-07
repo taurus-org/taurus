8757ac6ffb7481bf2f2db56764c8df0d6c0f73ae
cpascual <cpascual@cells.es>
2017-10-07 15:00:12 +0200

    Enable unit test for write/read epics attribute
    The epics.AttributeTestCase.write_read_attr was disabled because
    epics was not working in the CI test docker images.
    Reactivate it because epics seems to work well now.

--- lib/taurus/core/epics/test/test_epicsattribute.py
+++ lib/taurus/core/epics/test/test_epicsattribute.py
@@ -54,9 +54,7 @@ from taurus.core.taurusbasetypes import TaurusAttrValue
                                 wvalue=Quantity('1m'),
                                 quality=AttrQuality.ATTR_VALID,
                                 error=None,
-                                ),
-            test_skip='There are troubles in the docker container. '
-                      'This test will be skipped till we fix it'
+                                )
             )
 @unittest.skipIf(sys.modules.has_key('epics') is False,
                  "epics module is not available")
