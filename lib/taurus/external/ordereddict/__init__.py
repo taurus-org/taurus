b8ac5554ae2a26f290978c3031427d2815758922
Jan Kotanski <jkotan@mail.desy.de>
2017-10-26 09:21:36 +0200

    add depreacated warning

--- lib/taurus/external/ordereddict/__init__.py
+++ lib/taurus/external/ordereddict/__init__.py
@@ -1,8 +1,29 @@
-from __future__ import absolute_import
+# -*- coding: utf-8 -*-
 
-try:
-    # ordereddict from python 2.7 or from ordereddict installed package?
-    from collections import OrderedDict
-except ImportError:
-    # ordereddict from local import
-    import os
+##############################################################################
+##
+## This file is part of Taurus
+##
+## http://taurus-scada.org
+##
+## Copyright 2011 CELLS / ALBA Synchrotron, Bellaterra, Spain
+##
+## Taurus is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Lesser General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## Taurus is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU Lesser General Public License
+## along with Taurus.  If not, see <http://www.gnu.org/licenses/>.
+##
+##############################################################################
+
+from collections import OrderedDict
+from taurus.core.util import log as __log
+
+__log.deprecated(dep='taurus.external.ordereddict', rel='4.0.4')
