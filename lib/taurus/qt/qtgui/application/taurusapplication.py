877ed337ae2dcc1f28aaa6b9a5ae81b9ed28de93
Alexander Senchenko <a.i.senchenko@inp.nsk.su>
2018-07-18 16:13:38 +0700

    Revert "Mutliple creation-deletion of taurus application lead to sigfault."
    This reverts commit 262977ef80923ac1d1516c91217611f73ce2535d.
    This commit was accidently added to PR.

--- lib/taurus/qt/qtgui/application/taurusapplication.py
+++ lib/taurus/qt/qtgui/application/taurusapplication.py
@@ -176,7 +176,6 @@ class TaurusApplication(Qt.QApplication, Logger):
     For more details on taurus command line parsing check
     :mod:`taurus.core.util.argparse`.
     """
-    __app = None
 
     def __init__(self, *args, **kwargs):
         """The constructor. Parameters are the same as QApplication plus a
@@ -259,7 +258,6 @@ class TaurusApplication(Qt.QApplication, Logger):
         self.__registerQtLogger()
         self.__registerExtensions()
         self.__redirect_std()
-        TaurusApplication.__app = self
 
     def __registerQtLogger(self):
         import taurus.qt.qtcore.util
@@ -375,10 +373,3 @@ class TaurusApplication(Qt.QApplication, Logger):
         from taurus import info
         info('\n*********************\n%s', _DEPRECATION_COUNT.pretty())
         return ret
-    
-    counter = 0
-    @staticmethod
-    def instance(*args):
-        print('XXX',TaurusApplication.counter)
-        TaurusApplication.counter +=1
-        return Qt.QApplication.instance(*args)
