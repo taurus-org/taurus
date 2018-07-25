cd54db5d6f94ce049742330af6cfbdcc58a26286
Carlos Pascual <cpascual@cells.es>
2018-07-25 10:20:05 +0200

    Remove shortcut to change tango host action
    Remove the shortcut, since the action is deprecated. This leaves the action 
    accessible programmatically only.

--- lib/taurus/qt/qtgui/container/taurusmainwindow.py
+++ lib/taurus/qt/qtgui/container/taurusmainwindow.py
@@ -387,7 +387,6 @@ class TaurusMainWindow(Qt.QMainWindow, TaurusBaseContainer):
         self.quitApplicationAction.triggered[()].connect(self.close)
         self.changeTangoHostAction = Qt.QAction(Qt.QIcon.fromTheme(
             "network-server"), 'Change Tango Host ...', self)
-        self.changeTangoHostAction.setShortcut(Qt.QKeySequence("Ctrl+P"))
         self.changeTangoHostAction.triggered[()].connect(self._onChangeTangoHostAction)
         # make this action invisible since it is deprecated
         self.changeTangoHostAction.setVisible(False)
