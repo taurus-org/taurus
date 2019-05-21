9cb1f634f0ad7975a3336cd34d4f673118cb4f92
Carlos Pascual <cpascual@cells.es>
2019-05-21 15:03:14 +0200

    Simplify casting to int and avoid copy if possible
    The cast to ints when encoding non-scalars for tango is always 
    creating a copy. Avoid this copy whenever possible.

--- lib/taurus/core/tango/tangoattribute.py
+++ lib/taurus/core/tango/tangoattribute.py
@@ -417,10 +417,8 @@ class TangoAttribute(TaurusAttribute):
                 attrvalue = str(magnitude)
         elif fmt in (DataFormat._1D, DataFormat._2D):
             if PyTango.is_int_type(tgtype):
-                if not isinstance(magnitude, numpy.ndarray):
-                    magnitude = numpy.array(magnitude)
                 # cast to integer because the magnitude conversion gives floats
-                attrvalue = magnitude.astype('int64')
+                attrvalue = numpy.array(magnitude, copy=False, dtype='int64')
             elif tgtype == PyTango.CmdArgType.DevUChar:
                 attrvalue = magnitude.view('uint8')
             else:
