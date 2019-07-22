taurus-org/taurus

commit 86b79ea006715709294b023eed053b077335d548

Fix TaurusForm "Set Format" option does not work in compact mode
The compact elements of a taurusform ignore the set format option.

Propagate the format when the read widget is compacted and vice versa.

Fix #787

lib/taurus/qt/qtgui/panel/taurusvalue.py
@@ -442,8 +442,9 @@ class TaurusValue(Qt.QWidget, TaurusBaseWidget):
         """
         Reimplemented to call onSetFormatter of the read widget (if provided)
         """
-        if hasattr(self._readWidget, 'onSetFormatter'):
-            return self._readWidget.onSetFormatter()
+        rw = readWidget(followCompact=True)
+        if hasattr(rw, 'onSetFormatter'):
+            return rw.onSetFormatter()
 
     def setFormat(self, format):
         """
@@ -1086,10 +1087,19 @@ class TaurusValue(Qt.QWidget, TaurusBaseWidget):
     def setCompact(self, compact):
         if compact == self._compact:
             return
+
+        # Backup the current RW format
+        rw = self.readWidget(followCompact=True)
+        format = rw.getFormat()
+
         self._compact = compact
         if self.getModel():
             self.updateReadWidget()
             self.updateWriteWidget()
+
+        # Apply the format to the new RW
+        rw = self.readWidget(followCompact=True)
+        rw.setFormat(format)
 
     def isCompact(self):
         return self._compact

lib/taurus/qt/qtgui/panel/taurusform.py
@@ -386,7 +386,7 @@ class TaurusForm(TaurusWidget):
         format = TaurusWidget.onSetFormatter(self)
         if format is not None:
             for item in self.getItems():
-                rw = item.readWidget()
+                rw = item.readWidget(followCompact=True)
                 if hasattr(rw, 'setFormat'):
                     rw.setFormat(format)
         return format
@@ -397,7 +397,7 @@ class TaurusForm(TaurusWidget):
         """
         TaurusWidget.setFormat(self, format)
         for item in self.getItems():
-            rw = item.readWidget()
+            rw = item.readWidget(followCompact=True)
             if hasattr(rw, 'setFormat'):
                 rw.setFormat(format)
 

commit 896c34664c3a0622c8d91aae0797f676ff418c3e

Fix issue when setting compact mode before model
Commit 86b79ea006 introduced a bug: the compact mode cannot be set
unless the model is set. This causes issues e.g. when loading a
configuration of a TaurusForm in which the form compact mode was
set to True.

The problem is due to an exception in TaurusValue.readWidget() and
can be reproduced with the following snippet:

```
app = TaurusApplication(cmd_line_parser=None)
w = TaurusForm()
w.setCompact(True)
w.setModel(['sys/tg_test/1/float_scalar'])
w.show()
sys.exit(app.exec_())
```

Make TaurusValue.readWidget() more robust.

lib/taurus/qt/qtgui/panel/taurusvalue.py
@@ -389,7 +389,7 @@ class TaurusValue(Qt.QWidget, TaurusBaseWidget):
         itself.
         '''
         if followCompact and self.isCompact():
-            return self._readWidget.readWidget
+            return getattr(self._readWidget, 'readWidget', self._readWidget)
         return self._readWidget
 
     def writeWidget(self, followCompact=False):