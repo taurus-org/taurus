435b6a4977975702415c8332ca327bc3cda5633b
Carlos Pascual <cpascual@cells.es>
2020-05-07 21:14:07 +0200

    Finish working version of test_lazymodule
    Define the entry point on-the-fly in order not to
    require an installed dummy package with defined
    entry pointsfor the lazymodule unit test

--- lib/taurus/core/util/test/test_lazymodule.py
+++ lib/taurus/core/util/test/test_lazymodule.py
@@ -3,29 +3,38 @@ import pkg_resources
 from types import ModuleType
 from taurus.core.util.lazymodule import LazyModule
 
+from pkg_resources import EntryPoint, Requirement, WorkingSet
 
-def test_lazy_import_entry_point():
-    modname = "foobartest"
-    pkg_name = "taurus.core.util.test"
-    ep = list(pkg_resources.iter_entry_points('taurus.foobartest'))[0]
-    print(ep)
 
-    LazyModule.import_ep(modname, pkg_name, ep)
+def test_LazyModule():
 
-    # check that creating the LazyModule does not have side-efects
-    import taurus.core.util.test
+    # create an entry point for taurus.core.util.test.dumm
+    w = WorkingSet()
+    d = w.find(Requirement.parse('taurus'))
+    ep = EntryPoint.parse("dummy_mod = taurus.core.util.test.dummy", dist=d)
+    modname = ep.name
+
+    # lazy load the ep module as taurus.fbt
+    LazyModule.import_ep(modname, "taurus", ep)
+
+    # check that lazy-loading did not import the entry point modules
     assert modname not in sys.modules
+    assert ep.module_name not in sys.modules
 
     # import the module and check that it is a LazyModule
-    import taurus.core.util.test.foobartest as lzm
+    import taurus.dummy_mod as lzm
     assert isinstance(lzm, LazyModule)
 
-    # ask for a member of the lazy module
+    # same again
+    import taurus.dummy_mod as lzm
+    assert isinstance(lzm, LazyModule)
+
+    # now access a member of the lazy module
     assert lzm.foo == 1
 
-    # ...and check that any future import of lzm will import "real" module,
-    # not the lazy one
+    # ...and check that any subsequent import will return a "real" module,
+    # not a lazy one
 
-    import taurus.core.util.test.foobartest as lzm
+    import taurus.dummy_mod as lzm
     assert not isinstance(lzm, LazyModule)
     assert isinstance(lzm, ModuleType)
