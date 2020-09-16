61bf8a3dfc6508bc0b2ecd319757f5f790973edc
Carlos Pascual <cpascual@cells.es>
2020-09-16 12:07:24 +0200

    Declare support for python 3.8 in pypi classifiers

--- setup.py
+++ setup.py
@@ -166,6 +166,7 @@ classifiers = [
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'Topic :: Scientific/Engineering',
     'Topic :: Software Development :: Libraries',
     'Topic :: Software Development :: User Interfaces',
